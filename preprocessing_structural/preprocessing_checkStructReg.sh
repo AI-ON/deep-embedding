36a7d03f39990f4a6ef01ed90b5e073d710f8cae
Paul Zhutovsky <paul.zhutovsky@gmail.com>
2018-01-14 17:06:55 +0100

    very coarse check of registration in MNI 2mm space

---
+++ preprocessing_structural/preprocessing_checkStructReg.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+dataFolder="/data_remote/ABIDE/data"
+checksFolder="/data/pzhutovsky/deep-embedding/preprocessing_structural/checks_registration" 
+
+for subjFolder in ${dataFolder}/subject_*; do
+	echo ${subjFolder}
+	structFolder="${subjFolder}/anat"
+	structBrain="${structFolder}/anat_mni_2mm.nii.gz"
+	
+	fslmaths ${structBrain} -thrP 5 -bin ${structFolder}/mask_mni2mm
+done
+
+fslmerge -t ${checksFolder}/mask_merge.nii.gz `ls ${dataFolder}/*/anat/mask_mni2mm.nii.gz`
+fslmaths ${checksFolder}/mask_merge.nii.gz -TMean ${checksFolder}/maskAVG.nii.gz
+imrm ${checksFolder}/mask_merge.nii.gz
